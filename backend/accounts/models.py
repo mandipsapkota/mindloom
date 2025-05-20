206fcfa531ddb095af874ce940bfefb6c48a7e26
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-05-20 23:29:06 +0545

    test success

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class newModel(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
